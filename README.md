cc633ecbc41be127fd5fe24e2a514a0700d7f73c
maxall41 <maxall4@protonmail.com>
2023-04-26 17:50:20 -0700

    X

---
+++ README.md
@@ -0,0 +1,36 @@
+# Cylinder
+Cylinder is an embedded rust library for type-safe inter MCU/MPU communication 
+that supports `#![no_std]`. I created it because handling 
+complex inter-MCU communication without type-safety
+is extremely difficult, but with Cylinder you don't have
+to worry about defining unique `u8` values for 
+commands and conflicts and other issues with non type-safe communication.
+---
+Cylinder allows for 
+type-safe communication by providing a procedural macro that runs at build time that 
+you can apply to enum types. e.g:
+```rust
+use cylinder::CylinerBuildU8;
+
+#[derive(CylinerBuildU8)]
+enum Test {
+    MyEnum1,
+    MyEnum2,
+    MyEnum3,
+    MyEnum4
+}
+```
+At build time this enum get's converted to a hash map where each enum type
+gets mapped to a unique `u8` value that can be sent over I2C, SPI, CAN or any other protocol.
+You can use that hashmap by doing the below:
+```rust
+let hash : HashMap<&Test, u8> = Test::describe();
+```
+And then you can use it like this:
+```rust
+let val : u8 = hash.get(&Test::MyEnum1);
+```
+Note that to allow the usage of `#![no_std]` instead of using the `std:hashmap` implementation Cylinder uses
+hashbrown which is a hasmap library that supports `#![no_std]` so you will need to import `hashbrown` into your code to use the hashmap.
+Note that hashbrown when used in a `#![no_std]` enviroment requires that you configure a 
+global allocator with the `alloc` package for example with the `embedded-alloc` package.
--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,7 @@ extern crate rand;
 use std::panic;
 use proc_macro::{self, TokenStream};
 use quote::quote;
-use syn::{parse_macro_input, DataEnum, DataUnion, DeriveInput, FieldsNamed, FieldsUnnamed};
-use rand::Rng;
+use syn::{parse_macro_input, DataEnum,DeriveInput};
 use hashbrown::HashMap;
 
 
@@ -13,15 +12,16 @@ pub fn describe(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, data, .. } = parse_macro_input!(input);
     // Get Inital TokenStream
     let description = match data {
-        syn::Data::Struct(s) => panic!("Can not run on Structs!"),
+        syn::Data::Struct(_) => panic!("Can not run on Structs!"),
         syn::Data::Enum(DataEnum { variants, .. }) => {
             let vs  = variants.iter().map(|v| &v.ident);
             let x = quote! {#(#vs),*};
             format!("{}",x) // Almost certainly a better way todo this
         }
-        syn::Data::Union(DataUnion) => panic!("Can not run on Unions!"),
+        syn::Data::Union(_) => panic!("Can not run on Unions!"),
     };
     // Generate hash map
+    //TODO: Scrap gen hashmap and just reconstruct with sim vals
     let desv = description.split(",").collect::<Vec<&str>>();
     let mut hash: HashMap<&str, u8>  = HashMap::new();
     let mut i: u8 = 0;
@@ -33,58 +33,16 @@ pub fn describe(input: TokenStream) -> TokenStream {
         i += 1;
     }
     // Reconstruct hash map
-    let mut value_adds = String::from("let mut r_hash: HashMap<&str, u8>  = HashMap::new();");
+    let mut value_adds = format!("let mut r_hash: HashMap<&{}, u8>  = HashMap::new();",ident);
     for (key, value) in &hash {
-        value_adds = format!("{}\nr_hash.insert(\"{}\",{});",value_adds,key.trim(),value);
+        let r_key = format!("&{}::{}",ident,key.trim());
+        value_adds = format!("{}\nr_hash.insert({},{});",value_adds,r_key,value);
     }
     let sv: proc_macro2::TokenStream = value_adds.parse().unwrap();
     // Output hashmap
     let output = quote! {
         impl #ident {
-            fn describe() -> HashMap<&'static str, u8>  {
-                #sv
-                return r_hash
-            }
-        }
-    };
-
-    output.into()
-}
-
-#[proc_macro_derive(CylinerBuildU16)]
-pub fn describe(input: TokenStream) -> TokenStream {
-    let DeriveInput { ident, data, .. } = parse_macro_input!(input);
-    // Get Inital TokenStream
-    let description = match data {
-        syn::Data::Struct(s) => panic!("Can not run on Structs!"),
-        syn::Data::Enum(DataEnum { variants, .. }) => {
-            let vs  = variants.iter().map(|v| &v.ident);
-            let x = quote! {#(#vs),*};
-            format!("{}",x) // Almost certainly a better way todo this
-        }
-        syn::Data::Union(DataUnion) => panic!("Can not run on Unions!"),
-    };
-    // Generate hash map
-    let desv = description.split(",").collect::<Vec<&str>>();
-    let mut hash: HashMap<&str, u16>  = HashMap::new();
-    let mut i: u16 = 0;
-    for y in desv {
-        hash.insert(
-            y.trim(),
-            i,
-        );
-        i += 1;
-    }
-    // Reconstruct hash map
-    let mut value_adds = String::from("let mut r_hash: HashMap<&str, u16>  = HashMap::new();");
-    for (key, value) in &hash {
-        value_adds = format!("{}\nr_hash.insert(\"{}\",{});",value_adds,key.trim(),value);
-    }
-    let sv: proc_macro2::TokenStream = value_adds.parse().unwrap();
-    // Output hashmap
-    let output = quote! {
-        impl #ident {
-            fn describe() -> HashMap<&'static str, u8>  {
+            fn describe() -> HashMap<&'static #ident, u8>  {
                 #sv
                 return r_hash
             }
