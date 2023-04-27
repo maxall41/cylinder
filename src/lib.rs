5a0bd190d4fb75296015fffcc667ad41e6d4221c
maxall41 <maxall4@protonmail.com>
2023-04-26 18:54:21 -0700

    Updated documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+//! Cylinder is an embedded rust library for type-safe inter MCU/MPU communication that supports #![no_std]
+//! I created it because handling complex inter-MCU communication without type-safety is extremely difficult, but with
+//! Cylinder you don't have to worry about defining unique u8 values for commands, conflicts,
+//! and other issues with non type-safe communication.
 extern crate rand;
 
 use std::panic;
@@ -6,7 +10,23 @@ use quote::quote;
 use syn::{parse_macro_input, DataEnum,DeriveInput};
 use hashbrown::HashMap;
 
-
+/// Maps variants in enum to u8 values acsessible via a hashbrown hashmap
+/// the hashmap can be used like this:
+/// ```ignore
+/// use cylinder::CylinerBuildU8;
+///
+/// #[derive(CylinerBuildU8)]
+/// #[derive(Eq, Hash, PartialEq,Debug)]
+/// enum Test {
+///     MyEnum1,
+///     MyEnum2,
+///     MyEnum3,
+///     MyEnum4
+/// }
+/// let hash : HashMap<&Test, u8> = Test::describe();
+/// let val : u8 = hash.get(&Test::MyEnum1);
+/// ```
+/// Due note that when hashbrown is used in a `#![no_std]` enviroment you need to define a global allocator for the alloc crate such as `embedded-alloc`
 #[proc_macro_derive(CylinerBuildU8)]
 pub fn describe(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, data, .. } = parse_macro_input!(input);
